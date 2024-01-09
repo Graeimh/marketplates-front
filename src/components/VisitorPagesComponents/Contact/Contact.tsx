f1fdc59fa941a0f9d3a5ba2bb0f541a801225ebf
Graiemh <qg.guinier@gmail.com>
2024-01-09 15:30:45 +0100

    Added legal mentions

--- src/components/VisitorPagesComponents/Contact/Contact.tsx
+++ src/components/VisitorPagesComponents/Contact/Contact.tsx
@@ -32,14 +32,39 @@ function Contact() {
           <article className={styles.contactSections}>
             <h3>Legal mentions</h3>
             <p>
-              This website has been developped by Quentin Guinier, 23 Rue Jean
-              Jaurès, Lesquin, you can email me at the email address specified
-              above.
+              Conformément aux dispositions de la loi n° 2004-575 du 21 juin
+              2004 pour la confiance en l'économie numérique, il est précisé aux
+              utilisateurs du site Marketplates l'identité des différents
+              intervenants dans le cadre de sa réalisation et de son suivi.
             </p>
+            <h4>Edition du site</h4>
             <p>
-              Our map system is made using{" "}
-              <a href="https://react-leaflet.js.org/">React Leaflet</a>. We are
-              hosted by ...
+              Le présent site, accessible à l"URL
+              https://marketplates.netlify.app/ (le « Site »), est édité par :
+              Quentin Guinier, résidant 23 rue Jean Jaurès, de nationalité
+              Française (France), né(e) le 11/07/1996.
+            </p>
+            <h4>Hébergement</h4>
+            <p>
+              Le Site est hébergé par la société Netlify, situé 512 2nd Street,
+              Suite 200 San Francisco, CA 94107, (contact téléphonique ou email
+              : +14156911573).
+            </p>
+            <h4>Directeur de publication</h4>
+            <p>Le Directeur de la publication du Site est Quentin Guinier.</p>
+            <h4>Nous contacter</h4>
+            <ul>
+              <li>Par téléphone: +33620975571</li>
+              <li>Par email: marketplatesowner@gmail.com</li>
+              <li>Par courrier: 23 rue Jean Jaurès 59810 Lesquin</li>
+            </ul>
+            <h4>Données personnelles</h4>
+            <p>
+              Le traitement de vos données à caractère personnel est régi par
+              notre Charte du respect de la vie privée, disponible depuis la
+              section "Charte de Protection des Données Personnelles",
+              conformément au Règlement Général sur la Protection des Données
+              2016/679 du 27 avril 2016 («RGPD»).
             </p>
           </article>
         </div>
